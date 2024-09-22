28ce3b8e97ba51c00a3bad6dc440bde7913874c9
CHOI Seung Heon <139500661+hun7176@users.noreply.github.com>
2024-09-23 01:20:44 +0900

    Title: 금 채굴하기, Time: 33ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/금 채굴하기/gold-mining.cpp
+++ 240923/금 채굴하기/gold-mining.cpp
@@ -13,7 +13,7 @@ int cal_gold_cost(int i, int j, int k){
     int count_one=0;
     for(int x=i-k;x<=i+k;x++){
         if(x<1 || x>20) continue;
-        int sub_count=(k-abs(i-x));
+        int sub_count=abs(k-abs(i-x));
         for(int y=j-sub_count;y<=j+sub_count;y++){
             if(y<1 || y>20) continue;
             if(arr[x][y]==1){
@@ -24,6 +24,8 @@ int cal_gold_cost(int i, int j, int k){
     }
     return count_one;
 }
+
+//111 넣었다고 가정!
 
 
 int main(){
@@ -41,14 +43,14 @@ int main(){
         for(int j=1;j<=n;j++){
             //cout<<"현재좌표 : arr["<<i<<"]["<<j<<"]="<<arr[i][j]<<"\n";
             for(int k=0;k<=2*(n-1);k++){
-               // cout<<"현재 k : "<<k<<" ";
+            //    cout<<"현재 k : "<<k<<" ";
                 grid_cost=cal_grid_cost(k);
-                //cout<<"grid cost는 "<<grid_cost<<"\n";
+             //   cout<<"grid cost는 "<<grid_cost<<"\n";
                 gold_cost=cal_gold_cost(i,j,k);
-                //cout<<"gold cost는 "<<gold_cost<<"\n";
-                if(m*gold_cost-grid_cost>0){
+             //   cout<<"gold cost는 "<<gold_cost<<"\n";
+                if(m*gold_cost-grid_cost>=0){
                     max_gold=max(max_gold,gold_cost);
-                  //  cout<<"max_gold 는 "<<max_gold<<"\n";
+            //        cout<<"max_gold 는 "<<max_gold<<"\n";
                 }
             }
         }
